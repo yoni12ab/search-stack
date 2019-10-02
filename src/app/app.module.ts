205c906761ec339e6063385604b8936ab5e8f042
Yoni Argovi <yoni12ab@gmail.com>
2019-10-02 20:54:15 +0300

    added gitignore and fix local host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { SearchModule } from './components/search/search.module';
-import { HttpClientModule } from '@angular/common/http';
-
+import { AppComponent } from "./app.component";
+import { SearchModule } from "./components/search/search.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, SearchModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, SearchModule, HttpClientModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/search/search.service.ts
+++ src/app/components/search/search.service.ts
@@ -1,75 +1,78 @@
-import { Injectable } from '@angular/core';
-import {SearchApiService} from './search-api.service'
-import {TermModel} from './search-models'
-import { Subject, of, BehaviorSubject, combineLatest } from 'rxjs';
-import { debounceTime, distinctUntilChanged,switchMap, takeUntil, map} from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { SearchApiService } from "./search-api.service";
+import { Subject, of, BehaviorSubject, combineLatest } from "rxjs";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  map
+} from "rxjs/operators";
 
 @Injectable()
 export class SearchService {
-   private searchResults$ = new BehaviorSubject<any>([]);
-   private term$ = new BehaviorSubject<string>('');
-   private page$ = new BehaviorSubject<number>(1); 
-  
-   constructor(private searchApiService :SearchApiService) { 
-     this.init(); 
-   }
+  private searchResults$ = new BehaviorSubject<any>([]);
+  private term$ = new BehaviorSubject<string>("");
+  private page$ = new BehaviorSubject<number>(1);
 
-   public setTerm(term: string): void{
-     this.setPage(1); 
-     this.setResults([]);
-     this.term$.next(term); 
-   }
+  constructor(private searchApiService: SearchApiService) {
+    this.init();
+  }
 
-  public nextPage(): number{
+  public setTerm(term: string): void {
+    this.setPage(1);
+    this.setResults([]);
+    this.term$.next(term);
+  }
+
+  public nextPage(): number {
     const nextPage = this.page$.getValue() + 1;
-    this.setPage(nextPage); 
+    this.setPage(nextPage);
     return nextPage;
   }
-  
-  public clear(){
-    this.setTerm('');
+
+  public clear(): void {
+    this.setTerm("");
     this.setPage(1);
     this.setResults([]);
   }
 
-  private setPage(page = 1): void{
-     this.page$.next(page); 
+  public getResults(): Subject<any> {
+    return this.searchResults$;
   }
 
-  private setResults(res: any): void{
+  private setPage(page = 1): void {
+    this.page$.next(page);
+  }
+
+  private setResults(res: any): void {
     this.searchResults$.next(res);
   }
-  private appendResults(res: any){
+
+  private appendResults(res: any): void {
     let value = this.searchResults$.getValue();
-    if(value && value.items){
-        value.items.push(...res.items);
+    if (value && value.items) {
+      value.items.push(...res.items);
     } else {
-        value = res;
+      value = res;
     }
     this.searchResults$.next(value);
   }
 
-   public getResults(): Subject<any>{
-      return this.searchResults$;
-   }
+  private init() {
+    this.subscribeToTermChangeOnce();
+  }
 
-   private init(){
-      this.subscribeToTermChangeOnce();
-   }
-
-   private subscribeToTermChangeOnce(): void{
-     combineLatest(this.term$,this.page$)
+  private subscribeToTermChangeOnce(): void {
+    combineLatest(this.term$, this.page$)
       .pipe(
-            debounceTime(500),
-            distinctUntilChanged(),
-            switchMap(([term , page = 1]) => {
-              console.log(term);
-              return !term  ? of([]) : this.searchApiService.search(term, page)
-                }
-              ),
-              map(res => this.appendResults(res))
-          ).subscribe(); 
-   } 
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap(([term, page = 1]) => {
+          console.log(term);
+          return !term ? of([]) : this.searchApiService.search(term, page);
+        }),
+        map(res => this.appendResults(res))
+      )
+      .subscribe();
+  }
 }
-
-
